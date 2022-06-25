f69be2b99692e5bd3e7c066431f8c7a7437a8c5f
Chaz "Gamerappa" Péloquin <chazpelo2@gmail.com>
2022-06-24 20:16:57 -0400

    clean shit

--- conf/config.sample.php
+++ conf/config.sample.php
@@ -1,11 +1,8 @@
 <?php
 $host = '127.0.0.1';
-$db   = 'codename-web';
+$db   = 'heelercrap';
 $user = '';
 $pass = '';
-
-$tplCache = 'templates/cache';
-$tplNoCache = false; // **DO NOT SET AS TRUE IN PROD - DEV ONLY**
 
 // Array of memcached server(s) for memcache caching. Leave empty to disable memcache caching.
 $memcachedServers = [];
